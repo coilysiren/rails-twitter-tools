63fe4b714325da35f07559293ad9d440c44dad0e
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-14 19:59:39 -0700

    cleanup routes, add POST /mute

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root 'index#index'
+  post '/mute', to: 'index#mute'
 
-  get '/auth/:provider/callback', to: 'application#login'
-  get '/logout' => 'application#logout'
+  get  '/auth/:provider/callback', to: 'application#login'
+  get  '/logout', to: 'application#logout'
 
 end
