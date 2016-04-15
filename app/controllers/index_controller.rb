1b2f035135068b17a1d1a010acac63d70ee9afaa
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-14 20:00:01 -0700

    add mute controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,4 +3,14 @@ class IndexController < ApplicationController
   def index
   end
 
+  def mute
+    if current_user
+      target = params['target']
+      Action.mute(current_user, target)
+      redirect_to '/', notice: "Muted #{target}"
+    else
+      redirect_to '/', notice: 'Invalid Login'
+    end
+  end
+
 end
