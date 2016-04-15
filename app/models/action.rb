3f992869f554638b86d5b1417644753f77585641
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-14 22:04:58 -0700

    fix user_id being nil

--- app/models/action.rb
+++ app/models/action.rb
@@ -6,7 +6,7 @@ class Action < ActiveRecord::Base
   validates_uniqueness_of :target, scope: :user
 
   def self.mute(user, target)
-    Action.create!('user': user, 'target': target)
+    Action.create!(user_id: user.id, target: target)
     TwitterClient.create_user(user).mute(target)
     return "Muted #{target}"
   rescue ActiveRecord::RecordInvalid
