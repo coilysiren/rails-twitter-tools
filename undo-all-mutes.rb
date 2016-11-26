623ae6cfa52bd079e3d98a8d8574fa0e0c5c51e0
Lynn Cyrin <firemagelynn@gmail.com>
2016-11-25 18:06:00 -0800

    rescue from invalid or expired token

--- undo-all-mutes.rb
+++ undo-all-mutes.rb
@@ -3,7 +3,7 @@ for mute in Action.all()
     client = TwitterClient.create_user(mute.user)
     client.unmute(mute.target)
     mute.delete()
-  rescue NoMethodError, Twitter::Error::Forbidden
+  rescue NoMethodError, Twitter::Error::Forbidden, Twitter::Error::Unauthorized
     nil
   end
 end
